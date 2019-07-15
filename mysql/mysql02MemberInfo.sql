86acd8b63b04c91639f038cca8437468c0ac7ddc
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-07-15 23:34:05 +0900

    Update mysql02MemberInfo.sql

--- mysql/mysql02MemberInfo.sql
+++ mysql/mysql02MemberInfo.sql
@@ -2,16 +2,17 @@
 
 -- 테이블 생성
 create table memberinfo (
-	
+	idx int(7) not null auto_increment,
 	userid varchar(12) not null, 
     userpw varchar(16) not null,
     username varchar(20) not null,
     userphoto varchar(30), 
-    regdate datetime default CURRENT_TIMESTAMP
+    regdate datetime default CURRENT_TIMESTAMP,
+    PRIMARY KEY (idx)
 );
 
 -- 테이블 데이터 확인 
 desc memberinfo;
 select * from memberinfo;
-select * from memberinfo where ;
-
+select * from memberinfo where userid="1@1";
+drop table memberinfo;
